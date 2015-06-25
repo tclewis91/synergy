bfc75830264306390337dc7243130d24c00c21da
tclewis91 <tclewis91@gmail.com>
2015-06-25 14:07:08 -0500

    faker updated and working

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,18 @@
 require 'faker'
 5.times do
-  company =Company.create(name: Faker::Company.name)
+  company = Company.create(name: Faker::Company.name)
   5.times do
-    10.times do
-      company =company.employees.create(name: Faker::Name.name)
+    2.times do
+      employees = company.employees.create(name: Faker::Name.name)
       location = company.locations.create(city: Faker::Address.city)
        5.times do
          location.courses.create(title: Faker::Company.bs,
                               enrollment_cap: Array(4..80).sample)
-         2.times do
-          employees = employees.name.
-
+        2.times do
+           ids = location.employees.create(name: Faker::Name.name, company_id: company.id, location_id: location.id)
+            EmployeeCourse.create(employee_id: person.id, course_id: course.id)
+        end
+      end
     end
   end
 end
